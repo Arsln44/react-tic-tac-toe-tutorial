460964208dde9c3ffb4d7d447eb971d98805e5a0
Burak Alp Arslan <arslnburak2001@gmail.com>
2025-11-11 01:10:54 +0300

    Refactor: Render the board dynamically using .map()

--- src/App.js
+++ src/App.js
@@ -28,28 +28,29 @@ export default function Board() {
     const winner = calculateWinner(squares);
     let status;
     if (winner) {
-        status = "Winner" + winner;
+        status = "Winner: " + winner;
     } else {
         status = "Next Player: " + (xIsNext ? "X" : "O");
     }
     return (
         <>
             <div className="status">{status}</div>
-            <div className="board-row">
-                <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-                <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-                <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-            </div>
-            <div className="board-row">
-                <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-                <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-                <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-            </div>
-            <div className="board-row">
-                <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-                <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-                <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-            </div>
+            {
+                [0,1,2].map((rowIndex) => (
+                    <div className="board-row" key={rowIndex}>
+                        {[0,1,2].map((colIndex) => {
+                            const squareIndex = rowIndex * 3 + colIndex;
+                            return(
+                                <Square
+                                    key={squareIndex}
+                                    value={squares[squareIndex]}
+                                    onSquareClick={() => handleClick(squareIndex)}
+                                />
+                            )
+                        })}
+                    </div>
+                ))
+            }
         </>
     );
 }
